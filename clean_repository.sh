1fe366745bc96c0984559892f07cec08c2df514a
El Che <mine.paillou@gmail.com>
2020-12-18 23:34:45 +0100

    nom de l'executable modifier alors on modifie ici egalement

--- clean_repository.sh
+++ clean_repository.sh
@@ -1,6 +1,6 @@
 FILE=/home/schwi/Documents/c_project/c_environment/
-if [ -f "main.out" ]; then
-	rm "main.out"
+if [ -f "enumerate_numbers" ]; then
+	rm "enumerate_numbers"
 else
 	echo "$FILE does not exist."
 fi
